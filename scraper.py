c9ef09a9f06e5b6784af8a7d273e9868c595f25c
zzhang0511 <73339188+zzhang0511@users.noreply.github.com>
2021-01-07 15:57:47 +0800

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -202,7 +202,6 @@ with open('data/brand_names.csv','w') as resultFile:
     for key, value in brand_names.items():
         wr.writerow([key.encode('utf-8'), value.encode('utf-8')])
 brand_urls = read_data('data/brand_urls.csv')
-n1, n2 = sys.argv[1], sys.argv[2]
 pages_list = scrape_first_page(brand_urls, 0, 372)
 print("Finished writing csv file...")
 # after brands are scraped...
