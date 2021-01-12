208afbeb0cc14e23d33457f1e3389257049ab4fe
zzhang0511 <zhaorui.zhang17@imperial.ac.uk>
2021-01-12 21:44:07 +0800

    new scraping files

--- scraper/distinct_ajax.py
+++ scraper/distinct_ajax.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[7]:
+# In[1]:
 
 
 # Switch to Chinese website instead
@@ -18,32 +18,21 @@ import mysql.connector
 from datetime import datetime
 
 
-# In[10]:
+# In[4]:
 
 
-def get_html(url):
+def get_ajax(url):
+    ajaxport = ''.join(re.findall(r"[0-9]+",url,re.S))
+    url_info = "https://www.nosetime.com/app/item.php?id={}".format(ajaxport)
     ua = UserAgent()
     headers = {'User-Agent': ua.random}
-    try:
-        response = requests.get('https://www.nosetime.com'+url, headers=headers)
-    except HTTPError as e:
-        print ("Url Can not be found")
-        return None
-    except requests.exceptions.Timeout:
-        print ("Timeout error") # Maybe set up for a retry, or continue in a retry loop
-        return None
-    except requests.exceptions.TooManyRedirects:
-        print ("TooManyRedirects error") # Tell the user their URL was bad and try a different one
-        return None
-    except requests.exceptions.RequestException as e:
-        print(e) # catastrophic error. bail.
-        return None
+    response = requests.get(url_info,headers=headers)
     if response.status_code != 200:
         response = 1
     return response
 
 
-# In[15]:
+# In[ ]:
 
 
 if __name__ == '__main__':
@@ -57,37 +46,34 @@ if __name__ == '__main__':
     collation = 'utf8mb4_general_ci'
     )
     cursor_query = db_connection.cursor(buffered=True)
-    query = ("SELECT distinct url FROM (SELECT url FROM perfume_html where html_status = 0) tmp") #28885
+    query = ("SELECT distinct url FROM (SELECT url FROM perfume_html where ajax_status = 0) tmp") #28885
     cursor_query.execute(query)
     records = cursor_query.fetchall()
     cursor_query.close()
-    mySql_update_query = """ UPDATE perfume_html SET html_status = 1 WHERE url = %s """
-    mySql_insert_query = """INSERT INTO perfume_html_distinct (nowdate, url, html) VALUES (%s, %s, %s) """
+    mySql_update_query = """ UPDATE perfume_html SET ajax_status = 1 WHERE url = %s """
+    mySql_insert_query = """ INSERT INTO perfume_ajax_distinct (nowdate, url, ajax) VALUES (%s, %s, %s) """
     count = 0
-    print("Inserting perfumes html to mysqldb...")
+    print("Inserting perfumes ajax to mysqldb...")
     for record in records:
-        res = get_html(record[0])
+        res = get_ajax(record[0])
         if res == 1:
             print("Blocked at #{} url...".format(count))
-            break
-        html_text = res.text
-        if html_text == None:
-            print("Get HTML break at #{} url.".format(count))
             break
         db_cursor = db_connection.cursor()
         db_cursor.execute(mySql_update_query, (record[0]))
         db_cursor.close()
         db_connection.commit()
+        res = res.json()
         current_Date = datetime.now()
         formatted_date = current_Date.strftime('%Y-%m-%d %H:%M:%S:%f')
-        insert_tuple = (formatted_date,record[0],html_text)
+        insert_tuple = (formatted_date,record[0],res)
         db_cursor = db_connection.cursor()
         db_cursor.execute(mySql_insert_query, insert_tuple)
         db_cursor.close()
         db_connection.commit()
         count += 1
         if count % 100 == 0:
-            print("Scraped {} pages html...".format(count))
+            print("Scraped {} pages ajax...".format(count))
     db_connection.close()
     print("MySQL connection is closed")
     print("Woohoo, done! Congrats!")
