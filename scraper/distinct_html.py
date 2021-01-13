43790f647e7dfcefd549864dfa3798b955018fad
zzhang0511 <73339188+zzhang0511@users.noreply.github.com>
2021-01-13 23:28:26 +0800

    Update distinct_html.py

--- scraper/distinct_html.py
+++ scraper/distinct_html.py
@@ -58,7 +58,7 @@ if __name__ == '__main__':
     print("Inserting perfumes html to mysqldb...")
     while True:
         cursor_query = db_connection.cursor()
-        cursor_query.execute(query)
+        cursor_query.execute(mySql_select_query)
         record = cursor_query.fetchone()
         cursor_query.close()
         res = get_html(record[0])
